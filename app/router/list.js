9ac1b9ee71dfe250c234f2cd684ebdbbeb2ae27b
thassiov <tvmcarvalho@gmail.com>
2015-06-20 06:35:34 -0300

    I made a wrong route setup

--- app/router/list.js
+++ app/router/list.js
@@ -4,32 +4,32 @@ var routerSearch = express.Router();
 var m_site = require('../models/m_site');
 
 routerSearch.
-	get('/search/name:name', function(req, res) {
+	get('/search/name/:name', function(req, res) {
 		var name = req.params.name;
 		res.send(name);
 	});
 
 
 routerSearch.
-	get('/search/url:url', function(req, res) {
+	get('/search/url/:url', function(req, res) {
 		var url = req.params.url;
 		res.send(url);
 	});
 
 routerSearch.
-	get('/search/category:category', function(req, res) {
+	get('/search/category/:category', function(req, res) {
 		var category = req.params.category;
 		res.send(category);
 	});
 
 routerSearch.
-	get('/search/license:license', function(req, res) {
+	get('/search/license/:license', function(req, res) {
 		var license = req.params.license;
 		res.send(license);
 	});
 
 routerSearch.
-	get('/search/status:status', function(req, res) {
+	get('/search/status/:status', function(req, res) {
 		var status = req.params.status;
 		res.send(status);
 	});
